ffd402d28fe9edffba2667d4059f697b4e0b0513
Leonardo de Lima Junior <juhhinx@gmail.com>
2022-05-03 12:01:38 -0300

    add exercise about List 4

---
+++ workspace_idp_labs/lab-java-collections/src/br/inatel/idplabs/SobreList_4.java
@@ -0,0 +1,22 @@
+package br.inatel.idplabs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SobreList_4 {
+	public static void main(String[] args) {
+
+		List<String> listaDeMoveis = new ArrayList<>();
+		listaDeMoveis.add("Cadeira");
+
+		List<String> listaDeMoveisBloqueada = Collections.unmodifiableList(listaDeMoveis);
+
+		boolean flagMesmaInstancia = listaDeMoveis == listaDeMoveisBloqueada;
+
+		System.out.println("São a mesma instância? " + flagMesmaInstancia);
+
+		listaDeMoveis.add("Armário");
+		listaDeMoveisBloqueada.add("Sofá"); //Aqui é lançada uma exception, pois é impossível setar um item em uma lista unmodifiable
+	}
+}
