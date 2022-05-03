a053c5910daacf49f00dfbc60c5fa6d8b1e0d1c2
Leonardo de Lima Junior <juhhinx@gmail.com>
2022-05-03 11:51:08 -0300

    add itens on list

--- workspace_idp_labs/MyRestAPI/src/main/java/br/inatel/myrestapi/MyRestAPI/services/CursoService.java
+++ workspace_idp_labs/MyRestAPI/src/main/java/br/inatel/myrestapi/MyRestAPI/services/CursoService.java
@@ -21,10 +21,14 @@ public class CursoService {
 		Curso curso1 = new Curso(1L, "Rest API", 30);
 		Curso curso2 = new Curso(2L, "Mockito", 70);
 		Curso curso3 = new Curso(3L, "Scrum", 10);
+		Curso curso4 = new Curso(4L, "Curso de Eclipse, Instrutor Eduardinho", 7);
+		Curso curso5 = new Curso(5L, "Spring Boot", 16);
 		
 		mapa.put(curso1.getId(), curso1);
 		mapa.put(curso2.getId(), curso2);
 		mapa.put(curso3.getId(), curso3);
+		mapa.put(curso4.getId(), curso4);
+		mapa.put(curso5.getId(), curso5);
 	}
 	
 	public List<Curso> listarCursos() {
